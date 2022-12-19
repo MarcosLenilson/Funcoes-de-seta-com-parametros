7df3c1f9b79cb61934f4b2f1f32456e91708da34
Marcos Lenilson Alves Duarte <janajesuslelemarcos@gmail.com>
2022-12-18 21:12:47 -0300

    Add files via upload

---
+++ funções de seta com parâmetros.js
@@ -0,0 +1,8 @@
+// Funções de seta com parâmetros
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
